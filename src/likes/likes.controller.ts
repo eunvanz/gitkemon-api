92428bbd9385b75814b9e7aab29d374111813d3a
Benjamin <eunvanz@naver.com>
2021-08-29 11:00:33 +0900

    feat: add get/post like controller

--- src/likes/likes.controller.ts
+++ src/likes/likes.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Headers, Param, Post } from '@nestjs/common';
+import { ACCESS_TOKEN_HEADER_NAME } from 'src/constants/headers';
+import { CreateLikeDto } from './dto/create-like.dto';
+import { LikesService } from './likes.service';
 
 @Controller('likes')
-export class LikesController {}
+export class LikesController {
+  constructor(private readonly likeService: LikesService) {}
+
+  @Post()
+  async save(
+    @Headers(ACCESS_TOKEN_HEADER_NAME) accessToken: string,
+    @Body() createLikeDto: CreateLikeDto,
+  ) {
+    await this.likeService.save(accessToken, createLikeDto);
+  }
+
+  @Delete('/:likeId')
+  async delete(
+    @Headers(ACCESS_TOKEN_HEADER_NAME) accessToken: string,
+    @Param('likeId') likeId: number,
+  ) {
+    await this.likeService.delete(accessToken, likeId);
+  }
+}
